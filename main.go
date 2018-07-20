9dcd50b93cc0c93baa61dfc091f0b79cd5a0d326
cpluspluscrowe <ccrowe@unomaha.edu>
2018-07-20 15:57:23 +0000

    get the first item from db

--- main.go
+++ main.go
@@ -1,13 +1,12 @@
 package main
 
 import (
+	"fmt"
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/sqlite"
 )
 
 type Highlight struct {
-	gorm.Model
-	Id       int `gorm:"primary_key"`
 	Text     string
 	Url      string
 	BookName string
@@ -26,6 +25,8 @@ func main() {
 	db.Create(&Highlight{Text: "Storing ORM data", Url: "www.google.com", BookName: "book of hard knocks", IsPosted: 0})
 
 	var highlight Highlight
+	db.First(&highlight)
+	fmt.Println(highlight)
 	//	db.First(&highlight, 1)                     // find product with id 1
 	//	db.First(&highlight, "bookname = ?", "yes") // find product with code l1212
 
